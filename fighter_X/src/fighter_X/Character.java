9783f74452bcffccf9fe71d09527a15ed2f43642
jamlin416 <jamlin45522045@gmail.com>
2024-03-25 15:29:01 -0400

    1p Character ground movement

--- fighter_X/src/fighter_X/Character.java
+++ fighter_X/src/fighter_X/Character.java
@@ -1,13 +1,62 @@
 package fighter_X;
 
-public class Character {
+import javafx.scene.input.KeyCode;
+public class Character extends Hurtbox{
 	
-	private Hurtbox hb;
 	private int hp;
+	private int ground;
 	
-	public Hurtbox getHurtbox() {
-		return this.hb;
+	private int facing_left;
+	private int move_direction;
+	private double speed;
+	
+	public Character(double x, double y, double width, double height) {
+		this(x, y, width, height, 40);
 	}
+	
+	public Character(double x, double y, double width, double height, int ground) {
+		super(x, y, width, height);
+		this.ground = ground;
+		hp = 100;
+		
+		facing_left = -1; //1 for right, -1 for left
+		move_direction = 0; //1 for right, 0 for idle, -1 for left
+		
+		this.setOnKeyPressed(e->{
+			switch (e.getCode()) {
+			case A:
+				move_direction = -1;
+				
+				break;
+			case D:
+				move_direction = 1;
+				
+				break;
+			default:
+				break;
+			}
+		});
+		this.setOnKeyReleased(e->{
+			switch(e.getCode()) {
+			case A:
+				move_direction = move_direction==-1 ? 0 : move_direction; 
+				
+				break;
+			case D:
+				move_direction = move_direction==1 ? 0 : move_direction; 
+				
+				break;
+			default:
+				break;
+			}
+		});
+	}
+	
+	
+	public void update() {
+		this.setX(this.getX()+5*move_direction);
+	}
+
 	
 	public void takeDamage(double d) {
 		hp -= d;
