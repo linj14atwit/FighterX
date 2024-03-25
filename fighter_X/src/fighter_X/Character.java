e97638ec08b0299d00d8817d9c887b088072a0d9
jamlin416 <jamlin45522045@gmail.com>
2024-03-25 17:47:55 -0400

    Basic Implementation of attack

--- fighter_X/src/fighter_X/Character.java
+++ fighter_X/src/fighter_X/Character.java
@@ -1,8 +1,11 @@
 package fighter_X;
 
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+
 public class Character extends Hurtbox{
 	
+	private Pane parent; 
 	private int hp;
 	private int ground;
 	
@@ -10,28 +13,40 @@ public class Character extends Hurtbox{
 	private int move_direction;
 	private double speed;
 	
-	public Character(double x, double y, double width, double height) {
-		this(x, y, width, height, 40);
+	private Move current = null;
+	private Move light;
+	
+	public Character(Pane parent, double x, double y, double width, double height) {
+		this(parent, x, y, width, height, 40);
 	}
 	
-	public Character(double x, double y, double width, double height, int ground) {
+	public Character(Pane parent, double x, double y, double width, double height, int ground) {
 		super(x, y, width, height);
 		this.ground = ground;
-		hp = 100;
+		hp = 1000;
 		
 		facing_left = -1; //1 for right, -1 for left
 		move_direction = 0; //1 for right, 0 for idle, -1 for left
 		
+		light = new Move(parent, Attack.NORMAL, 30, 12, 60, 50, 30, 10);
+		
+		//inputs
 		this.setOnKeyPressed(e->{
 			switch (e.getCode()) {
 			case A:
 				move_direction = -1;
-				
 				break;
+				
 			case D:
 				move_direction = 1;
-				
 				break;
+			
+			case J:
+				light.doAttack(this.getX(), this.getY());
+				current = light;
+				System.out.println("light");
+				break;
+				
 			default:
 				break;
 			}
@@ -40,12 +55,12 @@ public class Character extends Hurtbox{
 			switch(e.getCode()) {
 			case A:
 				move_direction = move_direction==-1 ? 0 : move_direction; 
-				
 				break;
+				
 			case D:
 				move_direction = move_direction==1 ? 0 : move_direction; 
-				
 				break;
+			
 			default:
 				break;
 			}
@@ -53,8 +68,9 @@ public class Character extends Hurtbox{
 	}
 	
 	
-	public void update() {
+	public void update(long deltaTime) {
 		this.setX(this.getX()+5*move_direction);
+		if(current != null) current.update(deltaTime);
 	}
 
 	
--- fighter_X/src/fighter_X/Hitbox.java
+++ fighter_X/src/fighter_X/Hitbox.java
@@ -1,5 +1,7 @@
 package fighter_X;
 
+import javafx.scene.paint.Color;
+
 /**
  * @author James Yu Lin
  * A hitbox is a collider that will check if it collides with a hurtbox
@@ -9,6 +11,7 @@ public abstract class Hitbox extends Collider{
 	
 	public Hitbox(double x, double y, double width, double height){
 		super(x, y, width, height);
+		this.setFill(new Color(0.9, 0.4, 0.2, 0.4));
 	}
 	@Override
 	public boolean canCollide(Collider c) {
